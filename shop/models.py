997a53447541954a0f03d957807cd58b3f6ea613
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-03-08 07:33:08 +0000

    Разбор ORM
    - Исправления и улучшения
    - Обработчик каруселя (фронт)
    - Валидаторы

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator, FileExtensionValidator
-from my_utils.utils import validate_image, get_file_path
 from pytils.translit import slugify
 from django.core.validators import ValidationError
 from PIL import Image
 
+from my_utils.utils import validate_image, get_file_path
+from shop.validators import ProductTitleValidator
+
 
 class Category(models.Model):
-    title = models.CharField(_('Название'), max_length=255, unique=True)
+    title = models.CharField(
+        _('Название'),
+        max_length=255,
+        unique=True
+    )
     slug = models.SlugField(max_length=50)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -26,25 +32,40 @@ class Product(models.Model):
         'Название',
         max_length=255,
         unique=True,
-        validators=[RegexValidator(
-            regex=r'[^a-zA-ZА-Яа-яЁё0-9,.%*() ]',
-            message='Используйте буквы только латинского и русского алфавита',
-            inverse_match=True
-        )]
+        validators=[ProductTitleValidator()]
     )
-    slug = models.SlugField(max_length=50, unique=True, null=False)
-    description = models.TextField('Описание товара', max_length=600)
-    price = models.PositiveIntegerField(default=0)
-    sale_price = models.PositiveIntegerField(default=0, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    image = models.ImageField('Фото',
-                              upload_to=get_file_path,
-                              default='shop/default.jpg',
-                              validators=[validate_image,
-                                          FileExtensionValidator(
-                                              allowed_extensions=['jpg', 'png', 'jpeg'],
-                                              message='Данный формат файла не поддерживается', )]
-                              )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        null=False
+    )
+    description = models.TextField(
+        'Описание товара', max_length=600
+    )
+    price = models.PositiveIntegerField(
+        # TODO: Валидатор для обработки стоимости,
+        #  нельзя стоимость 0 указывать
+    )
+    sale_price = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    image = models.ImageField(
+        'Фото',
+        upload_to=get_file_path,
+        default='shop/default.jpg',
+        validators=[
+            validate_image,
+            FileExtensionValidator(
+                allowed_extensions=['jpg', 'png', 'jpeg'],
+                message='Данный формат файла не поддерживается',
+            )]
+    )
     is_available = models.BooleanField(default=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -56,11 +77,14 @@ class Product(models.Model):
 
     def clean(self):
         if self.sale_price >= self.price:
-            raise ValidationError(f'Цена со скидкой не может быть больше либо равна {self.price} руб.')
+            raise ValidationError(
+                f'Цена со скидкой не может быть больше либо равна '
+                f'{self.price} руб.'
+            )
+        return super().clean()
 
     def save(self, *args, **kwargs):  # new
         self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         width, height = img.size
@@ -70,13 +94,25 @@ class Product(models.Model):
             img.thumbnail(output_size)
             img.save(self.image.path)
 
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
 
 class Banner(models.Model):
-    image = models.ImageField('Банер категории', upload_to=get_file_path, default='shop/default.jpg')
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name='banners')
+    # TODO: название баннера
+    image = models.ImageField(
+        'Банер категории',
+        upload_to=get_file_path,
+        default='shop/default.jpg'
+    )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='banners'
+    )
 
     class Meta:
         verbose_name = 'Баннер'
--- shop/views.py
+++ shop/views.py
@@ -1,39 +1,70 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product, Category
+# Встроенные модули
+# import os
+# from abc import ABC
+
+# Внешние модули
 from django.db.models import F
-import math
+from django.shortcuts import get_object_or_404, redirect, render
+
+# Модули проекта
+from shop.models import Product, Category
 
 
 def index(request):
-    products = Product.objects.all().annotate(sale=(F('price') - F('sale_price')) * 100 / F('price'))
-    return render(request, 'shop/products.html', context={'products': products})
+    products = Product.objects.all().annotate(
+        sale=(F('price') - F('sale_price')) * 100 / F('price')
+    )
+    return render(
+        request,
+        'shop/products.html',
+        context={'products': products}
+    )
 
 
 def search_products(request):
-    if request.method == 'POST' and request.POST['searched'] != '':
+    if request.method == 'POST' and request.POST.get('searched'):
+        # r_search = request.POST.get('searched')
         r_search = request.POST['searched']
         products = Product.objects.filter(title__icontains=r_search)
-        return render(request, 'shop/search_products.html', context={'products': products, 'r_search': r_search})
-    return redirect('index', permanent=True)
+        return render(
+            request,
+            'shop/search_products.html',
+            context={'products': products, 'r_search': r_search}
+        )
+    # TODO: return redirect('index', permanent=True)
 
 
+# TODO: cat_selected возможно не используется
 def single_category(request, category_slug, cat_selected=0):
     category = get_object_or_404(Category, slug=category_slug)
     products_in_cat = category.products
     banners = category.banners.all()
+    # TODO: Возможно передаются ненужные данные в контексте запроса
     context = {
         'category': category,
         'products_in_cat': products_in_cat,
         'cat_selected': category.pk,
         'banners': banners,
     }
-    return render(request, 'shop/single_category.html', context=context)
+    return render(
+        request,
+        'shop/single_category.html',
+        context=context
+    )
 
 
 def single_product(request, product_slug):
     product = get_object_or_404(Product, slug=product_slug)
-    return render(request, 'shop/single_product.html', {'product': product})
+    return render(
+        request,
+        'shop/single_product.html',
+        {'product': product}
+    )
 
 
 def page_404(request, exception):
-    return render(request, 'shop/404.html', status=404)
+    return render(
+        request,
+        'shop/404.html',
+        status=404
+    )
